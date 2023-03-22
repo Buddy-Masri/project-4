6fb02c863c8a732cf63c7257c096494a8aaa3b96
“Buddy-Masri” <abdel.rahman.almasri@gmail.com>
2023-03-23 00:44:30 +0200

    save

---
+++ script.js
@@ -0,0 +1,35 @@
+let searchResult = document.getElementById('search-bar')
+let searchButton = document.getElementById('search-button')
+let cityName = document.getElementById('box-1-value')
+let WeatherStaus = document.getElementById('box-2-value')
+let temp = document.getElementById('box-3-main')
+let minTemp = document.getElementById('box-3-min')
+let maxTemp = document.getElementById('box-3-max')
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '2d3c54a101msh8b6b1b88083dac9p1879dbjsnd483a45ba7b0',
+		'X-RapidAPI-Host': 'openweather43.p.rapidapi.com'
+	}
+};
+
+
+const citySearch = () => {
+	fetch(`https://openweather43.p.rapidapi.com/weather?q=${searchResult.value}&appid=da0f9c8d90bde7e619c3ec47766a42f4&units=metric`, options)
+		.then(response => response.json())
+		.then(json => {
+			console.log(json)
+			cityName.innerHTML = json.name
+			WeatherStaus.innerHTML = json.weather[0].main
+			temp.innerText = `Temp: ${json.main.temp}℃`
+			minTemp.innerText = `Min Temp: ${json.main.temp_min}℃`
+			maxTemp.innerText = `Max Temp: ${json.main.temp_max}℃`
+		})
+	searchResult.value = null
+}
+
+searchButton.onclick = () => citySearch()
+
+
+
