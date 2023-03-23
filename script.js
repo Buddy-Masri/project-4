080a06c1c55427a44760c24fa349c9bdbc247541
“Buddy-Masri” <abdel.rahman.almasri@gmail.com>
2023-03-23 23:12:42 +0200

    save

--- script.js
+++ script.js
@@ -31,8 +31,27 @@ const options = {
 
 // searchButton.onclick = () => citySearch()
 
+// const citySearch = async () => {
+// 	let response = await fetch(`https://openweather43.p.rapidapi.com/weather?q=${searchResult.value}&appid=da0f9c8d90bde7e619c3ec47766a42f4&units=metric`, options)
+// 	let json = await response.json()
+// 		console.log(json)
+// 		searchResult.value = null
+// 		cityName.innerHTML = json.name
+// 		WeatherStaus.innerHTML = json.weather[0].main
+// 		temp.innerText = `Temp: ${json.main.temp}℃`
+// 		minTemp.innerText = `Min Temp: ${json.main.temp_min}℃`
+// 		maxTemp.innerText = `Max Temp: ${json.main.temp_max}℃`
+// }
+
+// searchButton.onclick = () => citySearch()
+
+
+let API_KEY = "a8e71c9932b20c4ceb0aed183e6a83bb";
+
 const citySearch = async () => {
-	let response = await fetch(`https://openweather43.p.rapidapi.com/weather?q=${searchResult.value}&appid=da0f9c8d90bde7e619c3ec47766a42f4&units=metric`, options)
+	const URL = "https://api.openweathermap.org/data/2.5/weather";
+	const FULL_URL = `${URL}?q=${searchResult.value}&appid=${API_KEY}&units=metric`;
+	let response = await fetch(FULL_URL)
 	let json = await response.json()
 		console.log(json)
 		searchResult.value = null
